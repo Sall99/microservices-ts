117223472f7953df61368d5003856cd0438b82af
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2021-11-15 12:54:02 +0000

    created auth service and seted up the server && infra/k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,9 +3,14 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+// ROUTES
+app.get('/api/users/currentuser', (req, res, next) => {
+   res.send("hi there  im the currentuser")
+})
+
 const Port = 3000
 
 app.listen(Port, () => {
-   console.log(`APP IS RUNNING ON PORT ${3000}`);
+   console.log(`APP IS RUNNING ON PORT ${3000}...`);
 
 })
